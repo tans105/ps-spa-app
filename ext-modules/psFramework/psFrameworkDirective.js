cf337f16083f0850a0e54fc2e8e01dfe5166b8fa
Tanmay <tanmayawasthi105@gmail.com>
2018-09-02 20:02:20 +0530

    Passing title,subtitle and img src using isolated scope

--- ext-modules/psFramework/psFrameworkDirective.js
+++ ext-modules/psFramework/psFrameworkDirective.js
@@ -3,7 +3,11 @@
 angular.module('psFramework').directive('psFramework', function () {
     return {
         transclude: true,
-        scope: {},
+        scope: {
+            title: '@',
+            subtitle: '@',
+            iconFile: '@'
+        },
         controller: 'psFrameworkController',
         templateUrl: 'ext-modules/psFramework/psFrameworkTemplate.html'
     }
