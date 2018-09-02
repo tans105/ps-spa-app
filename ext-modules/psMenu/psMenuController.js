971e4337a83785ab3cfb77fcb1a0e13a805f8e88
Tanmay <tanmayawasthi105@gmail.com>
2018-09-02 21:50:03 +0530

    Menu bar added without routing

---
+++ ext-modules/psMenu/psMenuController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('psMenu').controller('psMenuController',
+    ['$scope', function ($scope) {
+
+        }
+    ]);
---
+++ ext-modules/psMenu/psMenuItemDirective.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('psMenu').directive('psMenuItem',function(){
+    return{
+        require: '^psMenu',
+        templateUrl: 'ext-modules/psMenu/psMenuItemTemplate.html',
+        link: function(scope,el, attr, ctrl){
+
+        },
+        scope: {
+            label: '@',
+            icon: '@'
+        }
+    }
+});
