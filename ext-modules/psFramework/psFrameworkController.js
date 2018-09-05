fd0426128e9310603042c6ab81bd1207845d0447
Tanmay <tanmayawasthi105@gmail.com>
2018-09-05 15:30:55 +0530

    Menu button visible on screen width less than 768px only

--- ext-modules/psFramework/psFrameworkController.js
+++ ext-modules/psFramework/psFrameworkController.js
@@ -1,10 +1,27 @@
 'use strict';
 
 angular.module('psFramework').controller('psFrameworkController',
-    ['$scope',
-        function ($scope) {
+    ['$scope', '$window', '$timeout',
+        function ($scope, $window, $timeout) {
 
-            $scope.isMenuButtonVisible=true;
+            $scope.isMenuButtonVisible = true;
+
+            $($window).on('resize.psFramework', function () {
+                $scope.$apply(function () {
+                    checkWidth();
+                })
+            });
+
+            var checkWidth = function () {
+                var width = Math.max($($window).width(), $window.innerWidth);
+                $scope.isMenuVisible = (width > 768);
+                $scope.isMenuButtonVisible = !$scope.isMenuVisible;
+            };
+
+            $timeout(function(){
+                checkWidth();
+            }, 0);
+
             $scope.$on('ps-menu-item-selected-event', function (evt, data) {
                 $scope.routeString = data.route;
             });
